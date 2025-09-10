8909f459ec12d8f0f92e65fb98efdc4d6bdccd1e
Compulink-Dev <devcompulink@gmail.com>
2025-09-10 14:53:05 +0200

    deploy

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './styles.css'
 
 export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  description: 'Reserve Bank of Zimbabwe',
+  title: 'Reserve Bank of Zimbabwe',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
